62c37d60a14afadad4385a34b22718e48f4b3d3e
BINAH LOUIS <visteen192@gmail.com>
2022-11-24 12:54:19 +0000

    api home added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+def api_home(request, *args, **kwags):
+    return JsonResponse({"message:Hi there this is an API"},safe=False)
