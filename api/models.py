96addbc9f020c7b3c4cefecd5b1048dec0196a39
BINAH LOUIS <visteen192@gmail.com>
2022-11-30 10:38:54 +0000

    advocate model added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Advocate(models.Model):
+    username = models.CharField(max_length=200)
+    bio = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return self.username
