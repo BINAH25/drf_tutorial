e41331ab42f908bcb9f888db064d0add23ec6d08
BINAH LOUIS <visteen192@gmail.com>
2022-11-30 10:21:59 +0000

    additional two views added

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.api_home, name='api_home'),
+    path('advocate', views.advocate_list ),
+    path('advocate/<str:username>', views.advocate_detail),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 
-def api_home(request, *args, **kwags):
-    return JsonResponse({"message:Hi there this is an API"},safe=False)
+@api_view(['GET'])
+def api_home(request):
+    data = ['/advocate', 'advocate:username']
+    return Response(data)
+
+@api_view(['GET'])
+def advocate_list(request):
+    data = ['Dzodi', 'louis','mac']
+    return Response(data)
+
+@api_view(['GET'])   
+def advocate_detail(request,username):
+    data = username
+    return Response(data)
