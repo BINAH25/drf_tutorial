72dee90b8e6e899ef56f3eeaf5e9b83c097359c3
BINAH LOUIS <visteen192@gmail.com>
2022-12-01 07:35:06 +0000

    company url added

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    #TokenRefreshView,
+)
 
 urlpatterns = [
     path('', views.api_home, name='api_home'),
     path('advocate', views.advocate_list ),
     path('advocate/<str:username>', views.advocate_detail),
     path('company', views.company_list ),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 
 ]
